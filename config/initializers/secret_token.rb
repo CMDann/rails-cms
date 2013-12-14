29ae2121baebcd5e66c1c36367571048b5bc0dba
CMDann <CMDann.Development@gmail.com>
2013-12-13 21:01:39 -0600

    Added rails 3.2 files to the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCms::Application.config.secret_token = 'd57deba57b131004aeb5c287025afbff09c326189bca5c41fa1372d7ab6759e7cf13e3c7fe4870bf324c30b487a1af8eefb6bcd993b745d6c2c0878b1e0084a7'
